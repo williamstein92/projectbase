d76e20c9ab545ceeef1915d4bc576d4d6f0a7e0e
William Stein <williamstein92@gmail.com>
2014-06-19 17:33:54 -0400

    stuff

--- app.php
+++ app.php
@@ -1,3 +1,57 @@
 <?php
 
+abstract class Model extends Factory
+{
+  protected $propertybin;
 
+  public function __construct()
+  {
+    $this->populate();
+  }
+
+  abstract protected function inputs();
+
+  protected function populate()
+  {
+    foreach ($this->inputs() as $input) {
+      $this->has($input);
+    }
+  }
+
+  public function validate()
+  {
+    $passes = true;
+
+    foreach ($this->propertybin as $property)
+      $passes = $passes && $property->validate();
+
+    return $passes;
+  }
+
+  public function has($input)
+  {
+    $input = Input::get($input);
+    $this->propertybin[$input->name] = $input;
+    return $this;
+  }
+
+  public function __get($name)
+  {
+    return $this->propertybin[NameRater::forGets($name)]->value;
+  }
+
+  public function __set($name, $value)
+  {
+    $property = $this->propertybin[NameRater::forGets($name)];
+    if ($property) {
+      $tmp = $property->value;
+      $property->setValue($value);
+      if ( ! $property->validate()) {
+        $property->value = $tmp;
+        return false;
+      } else {
+        return $value;
+      }
+    }
+  }
+}
